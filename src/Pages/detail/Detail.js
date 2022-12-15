f8cb984989c5793684a2078a0faac2f0ea7f676a
erkiinov <erkinovbavrom12@gmail.com>
2022-12-15 19:31:07 +0500

    project two

--- src/Pages/detail/Detail.js
+++ src/Pages/detail/Detail.js
@@ -2,17 +2,56 @@ import React, {useEffect, useState} from 'react'
 import CardDetail from '../../components/detail/CardDetail'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import ProductInfo from '../../components/product-info/ProductInfo'
+import Title from '../../components/title/Title'
+import './style.scss'
+
+import Card from '../../components/card/Card'
 
 function Detail() {
   const params = useParams()
   const [api, setApi] = useState([{}])
+  const [cardAll, setCardAll] = useState([{}])
   axios.get(`https://fakestoreapi.com/products/${params.id}`).then(res=>setApi([res.data]))
+  axios.get(`https://fakestoreapi.com/products/`).then(res=>setCardAll(res.data))
   return (
     <div className='page1-container'>
-      {JSON.stringify(params)}
         {api.map(({id, title, price, description, category, image, rating}) => {
           return <CardDetail id={id} title={title} price={price} description={description} category={category} image={image} rating={rating} />
         })}
+          
+          
+        <div className='ProductInfo-div' style={{paddingTop: '40px'}}>
+
+          <div className='titleSearch' style={{width: '100%', borderBottom: '1px solid #EFEFEF', paddingBottom: '25px'}}><Title title='Product information.'/></div>
+          
+          <div className='grid-product-info'>
+
+            <ul>
+              <li><Title description='EEDRA Cilantro Seeds - contains 300 seeds in 1 Pack and professional instructions created by PhD Helga George'/></li>
+              <li><Title description='Be sure of our quality - the freshest batches of this season. Non-GMO, Heirloom - our seeds were tested and have the best germination ratings. Your easy growing experience is our guarantee'/></li>
+              <li><Title description='Cilantro common culinary uses: salsa, guacamole, pesto, salads, chutney, baked breads, pad thai, pico de gallo, rice, grilled shrimp skewers, falafel, and more'/></li>
+              <li><Title description='Proudly sourced in the USA - our garden seeds are grown, harvested, and packaged in the USA. We support local farmers and are happy to produce this American-made product'/></li>
+              <li><Title description='SEEDRA customer service - please contact us directly through Amazon with any questions or concerns about our products. We care about each customer and do our best to provide you with 100% satisfaction'/></li>
+            </ul>
+
+            <ProductInfo/>
+          </div>
+
+
+        </div>
+
+        <Title title='Related products.'/>
+
+        <div className='RetaletPr'>
+          {
+            cardAll.map(({id, title, price, description, category, image, rating}) => {
+              if(id > 1 && id <= 4){
+                return <Card img={image} title={title} starCount={rating.count} cost={price} id={id} />
+              } 
+            })
+          }
+        </div>
 
     </div>
   )
---
+++ src/components/product-info/ProductInfo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './style.scss'
+
+const ProductInfo = () => {
+  return (
+    <div className='product-info-uni'>
+        <div className='div'>
+            <h1>Package Dimensions</h1>
+            <h2>5.51 x 3.5 x 0.35 inches</h2>
+        </div>
+        <div className='div'>
+            <h1>Item Weight</h1>
+            <h2>0.317 ounces</h2>
+        </div>
+        <div className='div'>
+            <h1>ASIN</h1>
+            <h2>B08Z3HN5MP</h2>
+        </div>
+        
+    </div>
+  )
+}
+
+export default ProductInfo
